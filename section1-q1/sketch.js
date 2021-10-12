c1ad47e94ac3a7a35ddc5b6750a935bdfde4e396
Kai Washino <79495772+Kai-Washino@users.noreply.github.com>
2021-10-12 12:28:59 +0900

    Upgrade 1-q KU

--- section1-q1/sketch.js
+++ section1-q1/sketch.js
@@ -1,6 +1,6 @@
 // 練習問題：神戸市のマーク
 function setup(){
-  createCanvas(200, 200);
+  createCanvas(1000, 1000);
   background(255);
 
   // 空欄を埋めて神戸市のロゴマークを完成させよう
@@ -16,4 +16,27 @@ function setup(){
   const blue = color(14, 47, 146);
   const green = color(22, 131, 46);
   const gray = color(77);
+
+  strokeCap(ROUND);
+  strokeWeight(0);
+
+  fill(14, 47, 146);
+  //arc(100 + 25, 300, 100, 100, 0, PI * 2);
+  circle(110, 300, 100);
+
+  fill(255, 255, 255);
+  circle(102, 295, 95);
+
+  fill(22, 131, 46);
+  circle(100, 293, 90);
+
+  fill(255, 255, 255);
+  circle(98, 298, 86);
+
+  fill(110, 110, 110);
+  textStyle(BOLD);
+  textSize(22);
+  text("KOBE", 67, 290);
+  textSize(8);
+  text("UNIVERSITY", 80, 305);
 }
