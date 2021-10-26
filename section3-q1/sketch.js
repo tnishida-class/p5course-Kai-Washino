daa855420bf0934fc077f5bf97e9766b526c2c35
Kai Washino <79495772+Kai-Washino@users.noreply.github.com>
2021-10-26 12:19:52 +0900

    Change section3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -5,12 +5,19 @@ let cycle;
 function setup(){
   createCanvas(200, 200);
   count = 0;
-  cycle = 100;
+  cycle = 60;
 }
 
 function draw(){
   background(160, 192, 255);
   count = (count + 1) % cycle;
+  if(20 < count && count < 26){
+    size = 10 * (count - 20);
+  }
+
+  else{
+    size = 20;
+  }
   // BLANK[1]
   ellipse(width / 2, height / 2, size);
 }
