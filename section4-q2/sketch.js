9dd082dac5c77c3fbbb56f6045a0f51d58c95864
Kai Washino <79495772+Kai-Washino@users.noreply.github.com>
2021-11-02 10:55:46 +0900

    Change section4-q2

--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -12,7 +12,15 @@ function setup(){
 
   // 円グラフを描くには割合が必要なので合計を計算しておく
   let total = 0;
+  let percentage_old = 0;
+  let percentage = 0;
   for(let i = 0; i < scores.length; i++){ total += scores[i]; }
+  for(let i = 0; i < scores.length; i++){
+    percentage_old = percentage;
+    percentage += scores[i]/total;
+    arc(200, 200, 200, 200, 2 * PI * percentage_old, 2 * PI * percentage, PIE);
+  }
 
+  //arc(200, 200, 200, 200, 0, PI * 0.5, PIE);
   // BLANK[1]
 }
